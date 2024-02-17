4d42249f92309337e7693897ea82e1f0cd11bc1a
MonaMo02 <djeraouimouna@gmail.com>
2024-02-17 17:08:48 +0100

    first commit, started hero section

---
+++ src/Sections/Main/Hero.jsx
@@ -0,0 +1,25 @@
+
+export default function Hero(){
+    return(
+        <div className="herosec--main">
+            <div className="herosec--main--container">
+                <div className="herosec--main--content">
+                    <div className="herosec--main--title">
+                        <h1>Hello, I'm Danie Gold</h1>
+                        <h1> A UI/IX designer</h1>
+                    </div>
+                    <div className="herosec--main--description">
+                        <p className="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore doloribus, natus voluptas error quaerat, </p>
+                        <p className="text-sm">saepe sit delectus ipsum iusto aut corporis provident? Dicta explicabo rem dolores nostrum minima fugiat harum.</p>
+                    </div>
+                    <div>
+                        <button className="btn btn--explore">Explore</button>
+                    </div>
+                </div>
+                <div className="herosec--main--img">
+                    <img src="./img/picture1.png" alt="Hero" />
+                </div>
+            </div>
+        </div>
+    );
+}
